c0fe0a2579970cdb7c0282a6098f6be183563ad6
michele872 <michele.magurno@gmail.com>
2018-01-24 16:24:10 +0100

    sdkl

--- eclipse-workspace/Test1/src/inputOutput/RadingFromKeyboard.java
+++ eclipse-workspace/Test1/src/inputOutput/RadingFromKeyboard.java
@@ -1,10 +1,17 @@
 package inputOutput;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class RadingFromKeyboard {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter a line:");
+		System.out.println(stdin.readLine());
+		System.out.println("Enter an other line....:");
+		System.out.println(stdin.readLine());
 	}
 
 }
