67f54be254b0fdb05556d1fdb1cf3cbb7657a6ba
michele872 <michele.magurno@gmail.com>
2018-01-25 16:15:13 +0100

    classe SERIALIZE

--- eclipse-workspace/Test1/src/test/Message.java
+++ eclipse-workspace/Test1/src/test/Message.java
@@ -6,7 +6,7 @@ public class Message implements Serializable {
 	/**
 	 * 
 	 */
-	//private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	private String username;
 	private String textMessage;
 	
